464c9132850bb2bbb98e3b933473c00f37820af4
DanSheremeta <danylo3002@gmail.com>
2024-01-26 18:43:18 +0200

    add ordering for Position and TaskType models

--- tasks/models.py
+++ tasks/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(max_length=150)
 
+    class Meta:
+        ordering = ("name",)
+
     def __str__(self):
         return self.name
 
@@ -30,6 +33,9 @@ class Worker(AbstractUser):
 
 class TaskType(models.Model):
     name = models.CharField(max_length=150)
+
+    class Meta:
+        ordering = ("name",)
 
     def __str__(self):
         return self.name
