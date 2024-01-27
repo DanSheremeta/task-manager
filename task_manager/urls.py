423825f966795c440031648a7f1620f5bace51f6
DanSheremeta <danylo3002@gmail.com>
2024-01-27 16:44:09 +0200

    add django built-in auth

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tasks.urls", namespace="tasks")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
