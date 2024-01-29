d53defdd24ba3406b6f651cbba4275458ee9a8c0
DanSheremeta <danylo3002@gmail.com>
2024-01-29 17:41:45 +0200

    add to urls.py detail, create, update and delete urls for task

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,11 +2,19 @@ from django.urls import path
 
 from tasks.views import (
     TaskListView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
     WorkerRegistrationView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("register/", WorkerRegistrationView.as_view(), name="worker-registration"),
 ]
 
