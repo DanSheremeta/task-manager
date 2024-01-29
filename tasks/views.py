dedb37e4d97317c87e8910e7f8ad7131ea7afeb3
DanSheremeta <danylo3002@gmail.com>
2024-01-29 17:43:33 +0200

    add views for task detail, create, update and delete

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render, redirect
 from django.views import View
 
-from tasks.forms import WorkerCreationForm
+from tasks.forms import WorkerCreationForm, TaskForm
 from tasks.models import Task, Position
 
 
@@ -12,7 +13,28 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "task_list"
     template_name = "tasks/task_list.html"
-    paginate_by = 10
+    paginate_by = 4
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-detail")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:task-list")
 
 
 class WorkerRegistrationView(View):
