abbfb2495d750e450224ec7de6fdfb5e7497dd8d
DanSheremeta <danylo3002@gmail.com>
2024-02-15 04:39:54 +0200

    test new branch

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+# test text for testing new branch
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tasks.urls", namespace="tasks")),
