6ed37c99d7f2c217a2d6f6d5e1f2807642025097
DanSheremeta <danylo3002@gmail.com>
2024-02-16 12:38:11 +0200

    fix bug with DEBUG variable

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
+DEBUG = os.environ.get("DJANGO_DEBUG", "")
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
